b4c91abe99ada45a90d249267780e1ad0e5523eb
kindhtml <killpass1019@gmail.com>
2017-03-08 17:50:20 +0900

    gulpfile, package add

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 'use strict';
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var template = require('gulp-template-html');
+var connect = require('gulp-connect');
 
 var DIR = {
     SRC: 'src',
@@ -8,12 +11,37 @@ var DIR = {
 
 var SRC = {
     JS: DIR.SRC + '/js/*.js',
-}
+    CSS: DIR.SRC + '/css/*.css',
+    IMG: DIR.SRC + '/img/*.*',
+    SCSS: DIR.SRC + '/scss/*.scss',
+    HTML: DIR.SRC + '/*.html',
+    PAGES: DIR.SRC + '/pages/*.html'
+};
 
 var DIST = {
     JS: DIR.DIST + '/js',
-}
+    CSS: DIR.DIST + '/css',
+    IMG: DIR.DIST + '/img',
+    PAGES: DIR.DIST + '/pages',
+    HTML: DIR.DIST + '/'
+};
 
-gulp.task('default', function(){
+var condition = true;
+
+gulp.task('indexHTML', function(){        
+    return gulp.src(SRC.HTML)
+    .pipe(template('src/templates/template.html'))
+    .pipe(gulp.dest(DIST.HTML))
+    .pipe(connect.reload());
+});
+
+gulp.task('subHTML', function(){        
+    return gulp.src(SRC.PAGES)
+    .pipe(template('src/templates/template.html'))
+    .pipe(gulp.dest(DIST.PAGES))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['indexHTML', 'subHTML'], function(){
 
 });
