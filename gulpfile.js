496225a13ec2584c6f23e3875ee1a8d966d08227
kindhtml <killpass1019@gmail.com>
2017-03-08 13:06:50 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+var gulp = require('gulp');
+
+var DIR = {
+    SRC: 'src',
+    DIST: 'dist'
+};
+
+var SRC = {
+    JS: DIR.SRC + '/js/*.js',
+}
+
+var DIST = {
+    JS: DIR.DIST + '/js',
+}
+
+gulp.task('default', function(){
+
+});
